388728cfb546a6c11ae1d3a031bf64ee4b44b100
fjs138 <fjs138@gmail.com>
2016-04-20 21:04:12 -0400

    Wed Apr 20 21:04:12 EDT 2016

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ function($scope){
 	{title: 'post 3', upvotes: 15},
 	{title: 'post 4', upvotes: 9},
 	{title: 'post 5', upvotes: 4}
-  ];
-  $scope.addPost = function(){
-	$scope.posts.push({title: $scope.title, upvotes: 0});
-	scope.title = '';
+	];
+	$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
+  $scope.title = '';
+  $scope.link = '';
 };
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
 }]);
