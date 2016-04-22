8bc9d656a8c18d40be68ff3ba2e5f5ff5c28faa4
fjs138 <fjs138@gmail.com>
2016-04-21 23:27:36 -0400

    Thu Apr 21 23:27:36 EDT 2016

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// -------- Mongoose ----------
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');          // load post model
+var Comment = mongoose.model('Comment');    // load comment model
+
+// ----------- REST Routes --------------
+// Get all posts
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if (err) { return next(err); }
+		
+		res.json(posts);
+	});
+});
+
+// Create new post; making a POST request to the server 
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+
 module.exports = router;
